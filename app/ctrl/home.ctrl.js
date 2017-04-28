e88e2b0a66344deed0eedae08d39a09a6da95e58
oNguyenDinhTin <tinnd@fsoft.com.vn>
2017-04-28 20:14:17 +0900

    update home design

--- app/ctrl/home.ctrl.js
+++ app/ctrl/home.ctrl.js
@@ -7,18 +7,25 @@ angular.module('myApp.home', ['ngRoute'])
             templateUrl: 'view/home.view.html',
             controller: 'HomeCtrl',
             resolve: {
-                factory: function ($q, $rootScope, $location, $facebook) {
+                factory: function ($q, $rootScope, $location, $facebook, $timeout) {
                     var d = $q.defer();
                     var start = function () {
                         d.reject();
                         $location.path('/map');
                     };
                     $facebook.getLoginStatus().then(function (e) {
+                        $rootScope.connect = 'good';
                         if (e.status === 'connected') { start(); }
                         else { d.resolve(true); }
                     }, function () {
                         d.resolve(true);
                     });
+                    $timeout(function () {
+                        if (!window.FB) {
+                            $rootScope.connect = 'fail';
+                            d.resolve(true);
+                        }
+                    }, 5000);
                     return d.promise;
                 }
             }
