fced9e26ee11cd118c259523f882a3b378778278
oNguyenDinhTin <tinnd@fsoft.com.vn>
2017-05-04 00:39:01 +0900

    add socket test

--- app/ctrl/info.ctrl.js
+++ app/ctrl/info.ctrl.js
@@ -9,6 +9,59 @@ angular.module('myApp.info', ['ngRoute'])
         });
     }])
 
-    .controller('InfoCtrl', [function () {
+    .controller('InfoCtrl', function ($rootScope, $scope) {
+        $scope.messages = [];
+        $scope.chat = {
+            joined: false,
+            user: '',
+            content: '',
+            send: function (user, text) {
+                var msg = {
+                    user: user,
+                    text: text,
+                    time: new Date().getTime()
+                };
+                updateMessages(msg);
+                conn.send(angular.toJson(msg));
+                $scope.chat.content = '';
+            },
+            join: function (user) {
+                var msg = {
+                    user: user,
+                    text: user + ' entered the room',
+                    time: new Date().getTime()
+                };
+                updateMessages(msg);
+                conn.send(angular.toJson(msg));
+                $scope.chat.joined = true;
+            },
+            leave: function (user) {
+                var msg = {
+                    user: user,
+                    text: user + ' has left the room',
+                    time: new Date().getTime()
+                };
+                updateMessages(msg);
+                conn.send(angular.toJson(msg));
+                conn.close();
+                $scope.messages = [];
+                $scope.chat.content = '';
+                $scope.chat.joined = false;
+            }
+        };
 
-    }]);
+        function updateMessages(msg) {
+            $scope.messages.push(msg);
+        }
+
+        var conn = new WebSocket('ws://localhost:8080');
+        conn.onopen = function (e) {
+            console.log("Connection established!");
+        };
+
+        conn.onmessage = function (e) {
+            var msg = angular.fromJson(e.data);
+            updateMessages(msg);
+            $scope.$apply();
+        };
+    });
