ae4028bc0b3b6ab46513759dcc6df3a79ac9aa8f
Tin <nittin990@gmail.com>
2017-05-09 22:12:58 +0700

    rm secret

--- back/key.php
+++ back/key.php
@@ -5,11 +5,11 @@
  * Date: 4/4/14
  * Time: 12:04 PM
  */
-$_KEY_USERNAME = 'appadmin';
-$_KEY_PASSWORD = '00065407';
-$_KEY_DB = 'mangohihi';
+$_KEY_USERNAME = '';
+$_KEY_PASSWORD = '';
+$_KEY_DB = '';
 
-$_KEY_PUSHER_APP = '336747';
-$_KEY_PUSHER_AUTH = '507219bfa4b398d75502';
-$_KEY_PUSHER_SECRET = '1d61e0648f8a0efc01db';
-$_KEY_PUSHER_CLUSTER = 'ap1';
+$_KEY_PUSHER_APP = '';
+$_KEY_PUSHER_AUTH = '';
+$_KEY_PUSHER_SECRET = '';
+$_KEY_PUSHER_CLUSTER = '';
